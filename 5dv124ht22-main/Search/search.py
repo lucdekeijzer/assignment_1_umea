1aae5f76a830b637fbb97df5c79630a13feae2de
Luc de Keijzer <lucdekeijzer36@gmail.com>
2023-09-12 14:53:44 +0200

    Working DFS

--- 5dv124ht22-main/Search/search.py
+++ 5dv124ht22-main/Search/search.py
@@ -154,167 +154,31 @@ def aStarSearch(problem: SearchProblem, heuristic=nullHeuristic):
 
 # General search algorithm based on the example in the lecture
 def depthFirstSearch(problem: SearchProblem): #BFS
-    from game import Directions
-
-    print("-----------------------------------------------------------------------")
-    count = 1
-    visited_coor = []
-    visited_directions = []
-    fringe_coor = util.Queue()
-    fringe_directions = util.Queue()
-    path = ["South"]
-    
-    visited_coor.insert(0, problem.getStartState())
-    next_node = problem.getSuccessors(problem.getStartState())
-    print(f"{next_node=}")
-    print(f"{problem.isGoalState(problem.getStartState())=}")
-    # for i in range(len(next_node)):
-        
-    #     fringe_coor.push(next_node[i][0])
-    for i in range(len(next_node)):
-                        # HERE MAYBE?
-                        print(f"{next_node[i][0]=}")
-                        if next_node[i][0] not in visited_coor:
-                            temp_coor = next_node[i][0]
-                            temp_dir = next_node[i][1]
-                            print(f"{temp_coor=}, {temp_dir=}")
-                            print(f"running for loop check {i=} ,{next_node[i][0]}")
-                            fringe_directions.push(temp_dir)
-                            fringe_coor.push(temp_coor)
-                            # fringe_directions.push(next_node[i][1])
-                            # fringe_coor.push(next_node[i][0])    
-
-    
-    while True:
-        print(f"{visited_coor=}")
-        print(f"{visited_directions=}")
-        print("fringe coor")
-        fringe_coor.print()
-        print("fringe directions")
-        fringe_directions.print()
-        # print(f"{fringe_directions=}")
-
-        if fringe_coor.isEmpty():
-            # print("are we getting here?")
-            print("Fringe was empty")
-            for i in visited_directions:
-                if i == "South":
-                    path.insert(0, Directions.SOUTH)
-                elif i == "North":
-                    path.insert(0, Directions.NORTH)
-                elif i == "West":
-                    path.insert(0, Directions.WEST)
-                elif i == "East":
-                    path.insert(0, Directions.EAST)
-            print("do we get to the end?")
-            break         
-            # return path
-            # break
-        
-        else:
-            try:  
-                curr_node = fringe_coor.pop()
-                print(f"first else statement: {curr_node=}")
-                if problem.isGoalState(curr_node):
-                    print("are we getting to the goal?")
-                    print("THIS WILL BE PRINTED IF WE FOUND THE GOAL")
-                    for i in visited_directions:
-                        if i == "South":
-                            path.insert(0, Directions.SOUTH)
-                        elif i == "North":
-                            path.insert(0, Directions.NORTH)
-                        elif i == "West":
-                            path.insert(0, Directions.WEST)
-                        elif i == "East":
-                            path.insert(0, Directions.EAST)
-                            # return path
-                    break
-                else:
-                    print(f"{curr_node=}")
-                    next_node = problem.getSuccessors(curr_node)
-                    # Adding all the possible next coordinate states in the to do list
-                    print(f"{len(next_node)=}")
-                    print(f"{next_node=}")
-                    for i in range(len(next_node)):
-                        print(f"{next_node[i][0]=}")
-                        if next_node[i][0] not in visited_coor:
-                            temp_coor = next_node[i][0]
-                            temp_dir = next_node[i][1]
-                            print(f"{temp_coor=}, {temp_dir=}")
-                            print(f"running for loop check {i=} ,{next_node[i][0]}")
-                            fringe_directions.push(temp_dir)
-                            fringe_coor.push(temp_coor)
-                            # fringe_directions.push(next_node[i][1])
-                            # fringe_coor.push(next_node[i][0])
-
-                        # print(f"Next coordinates (state): {i}, {next_node[i][0]=}")
-                    visited_coor.insert(0, curr_node)
-                    last_direction = fringe_directions.pop()
-                    visited_directions.insert(0, last_direction)
-                    # fringe_coor.push(problem.getSuccessors(curr_node))
-                        
-            # except Exception:
-            #     print("Exception caught")        
-            
-            except IndexError:
-                print(f"We got an indexError")
-                for i in visited_directions:
-                    if i == "South":
-                        path.insert(0, Directions.SOUTH)
-                    elif i == "North":
-                        path.insert(0, Directions.NORTH)
-                    elif i == "West":
-                        path.insert(0, Directions.WEST)
-                    elif i == "East":
-                        path.insert(0, Directions.EAST)
-                break
-    
-        print(f"{count=}")
-        count = count + 1
-        print("------------------------------------------------------------------")
-    print("We should be getting to the end")
-    print(f"{type(visited_directions)=}")
-    print(f"{visited_directions=}")
-    visited_directions = visited_directions.reverse()
-    print(f"{path=}")
-    path.reverse()
-    print(f"{path=}")
- 
-    return path            
-
-
-    
-    #fringe ← Insert (Make-Node(Initial-Stat e [problem]), fringe)
-    # python3 pacman.py -l tinyMaze -p SearchAgent -a fn=depthFirstSearch
-
-def depth_first_search(problem):
-    # Initialize the stack with the start state and an empty path
-    # stack = [(problem.getStartState(), [])]
     stack = util.Stack()
-    stack.push(problem.getStartState())
-    # Create a set to keep track of visited states
+    start_node = problem.getStartState()
+    dir = []
+    cost = 0
+    stack.push((start_node, dir, cost))  
+
     visited = []
-    
-    while stack:
-        # Pop the current state and path from the stack
-        state, path = stack.pop()
+
+    while not stack.isEmpty():
+        # print(f"The stack looks like this{stack.pop()=}")
+        curr_node, dir, cost  = stack.pop()
+        print(f"{dir=}")
+        print(f"{curr_node=}")
+        if problem.isGoalState(curr_node):
+            print(f"End dir: {dir=}")
+            return dir
         
-        # Check if the current state is the goal state
-        if problem.isGoalState(state):
-            return path  # Return the path to the goal
-        
-        # Mark the current state as visited
-        visited.append(state)
-        
-        # Get the successor states and actions
-        successors = problem.getSuccessors(state)
-        
-        for next_state, action, _ in successors:
-            if next_state not in visited:
-                # Push the successor state and updated path onto the stack
-                stack.push((next_state, path + [action]))
-    
-    return None  # If no path to the goal is found, return None
+        visited.append(curr_node)
+
+        next_nodes = problem.getSuccessors(curr_node)
+        for node, next_dir, cost in next_nodes:
+            if node not in visited:
+                stack.push((node, dir + [next_dir], cost))
+
+    return None
 
 
 
